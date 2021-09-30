c78bb3b2879b1acfb036c6ae3fdf63653813cd58
vikramv29 <vikramvermavv24@gmail.com>
2021-09-30 23:22:00 +0530

    LatestCode

--- bus-hub-front-end/src/app/viewticket-by-id/viewticket-by-id.component.ts
+++ bus-hub-front-end/src/app/viewticket-by-id/viewticket-by-id.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Bus } from '../bus';
+import { Customer } from '../customer';
+import { Ticket } from '../ticket';
+import { TicketService } from '../ticket.service';
 
 @Component({
   selector: 'app-viewticket-by-id',
   templateUrl: './viewticket-by-id.component.html',
   styleUrls: ['./viewticket-by-id.component.css']
 })
-export class ViewticketByIdComponent implements OnInit {
+export class ViewticketByIdComponent {
 
-  constructor() { }
+  customerId: number;
+  customer: Customer;
+  busId: number;
+  bus: Bus;
+  ticketId: number;
+  ticket: Ticket;
+  flag: boolean;
+  viewflag:boolean;
 
-  ngOnInit() {
+  constructor(private service: TicketService, private router: Router ) { 
+    this.ticket = new Ticket();
+    this.flag = true;
+    this.viewflag = false;
   }
 
+  getTicketById() {
+    this.service.getTicketByticketId(this.ticketId).subscribe(res => {
+      // console.log(this.customer);
+      this.ticket = res;
+      // console.log(this.customer);
+      if(this.ticket == new Ticket()){
+        this.flag = false;
+    }
+      else{
+        this.viewflag = true;
+      }
+      
+    })
+  }
 }
